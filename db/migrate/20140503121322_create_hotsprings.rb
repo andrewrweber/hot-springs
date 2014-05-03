b9c8067cdebb2c9c07c0615103f33977f5f453c8
Andrew Weber <weber@mbp.local>
2014-05-03 14:43:47 -0700

    finish the database

--- db/migrate/20140503121322_create_hotsprings.rb
+++ db/migrate/20140503121322_create_hotsprings.rb
@@ -1,12 +1,14 @@
 class CreateHotsprings < ActiveRecord::Migration
   def change
     create_table :hotsprings do |col|
+      col.string :state_code
       col.string :state
       col.string :latitude
       col.string :longitude
-	  col.string :location
+	  col.string :location_url
       col.string :name
       col.string :temperature
+      col.string :comfort_level
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,14 @@ require 'csv'
 def seed_hotsprings
   CSV.foreach("db/hot_springs_list.csv") do |row|
     data = {}
+    data[:state_code] = row.shift
     data[:state] = row.shift
     data[:latitude] = row.shift
     data[:longitude] = row.shift
-    data[:location] = generate_location_url(data[:latitude],data[:longitude])
+    data[:location_url] = generate_location_url(data[:latitude],data[:longitude])
     data[:name] = row.shift
     data[:temperature] = row.shift
+    data[:comfort_level] = row.shift
 
     puts "Building hotsprings '#{data[:name]}'..."
     Hotspring.create(data)
