9c4309dc60e1b5839fd3eeff4d205b0ad6ecd3e3
John Olmsted <johnaolmsted@gmail.com>
2014-05-03 12:51:04 -0700

    Adds event handler for state click

--- public/js/map.js
+++ public/js/map.js
@@ -5,5 +5,9 @@ $(document).ready(function() {
 var Map = {
   init: function(){
     $('#map').vectorMap({map: 'us_aea_en'});
+    // Event handler for clicking a state:
+    $('#map').bind('regionClick.jvectormap', function(event, code) {
+      console.log(code);
+    });
   }
 }
